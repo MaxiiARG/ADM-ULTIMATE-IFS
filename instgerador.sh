79884e6080a52ac3f9e740c2b1bfb3d028403b54
MaxiiARG <40850209+MaxiiARG@users.noreply.github.com>
2019-11-18 18:14:09 -0300

    Update instgerador.sh

--- instgerador.sh
+++ instgerador.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IVAR="/etc/http-instas"
 SCPT_DIR="/etc/SCRIPT"
-SCPresq="aHR0cHM6Ly9naXRodWIuY29tL01heGlpQVJHL0FETS1VTFRJTUFURS1JRlMvdHJlZS9tYXN0ZXIvZ2VyYWRvcg=="
+SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL01heGlpQVJHL0FETS1VTFRJTUFURS1JRlMvdHJlZS9tYXN0ZXIvZ2VyYWRvcg=="
 SUB_DOM='base64 -d'
 rm $(pwd)/$0
 ofus () {
